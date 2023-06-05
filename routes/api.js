400fcb24443bdbf9a504288d958f2b8190e4ad67
luciaqu8 <luciaquinteiro8396@gmail.com>
2023-06-05 16:32:16 -0300

    ultimate commit

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
+
 
 router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -26,6 +28,32 @@ router.get('/novedades', async function (req, res, next) {
     });
 
     res.json(novedades);
-})
+});
+
+router.post('/contacto', async (req, res) => {
+
+    var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "585a9131d19479",
+          pass: "0e94ff09871b67"
+        }
+      });
+
+    const mail = {
+        to: 'luciaquinteiro8396@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre}' se contacto a traves de la web y quiere más información a este correo: ${req.body.email} 
+        <br> Además, hizo el siguiente comentario: ${req.body.mensaje}`
+    }
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
+});
 
 module.exports = router;
